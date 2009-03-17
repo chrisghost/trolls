316edd8022d23b72f21bbb634096ad764ad70c13
maillol.adrien <maillol.adrien@03b5efae-12cf-11de-99c7-9d618077940f>
2009-03-17 11:51:34 +0000

    k

---
+++ trunk/src/trolls/vueMap.java
@@ -0,0 +1,15 @@
+package trolls;
+
+public class vueMap {
+	
+	Map map;
+	
+	public vueMap(Map m) {
+		this.map = m;
+	}
+	
+	public void afficherMap() {
+		
+	}
+	
+}
---
+++ trunk/src/trolls/vueTroll.java
@@ -0,0 +1,25 @@
+package trolls;
+
+import torque.generated.Troll;
+
+public class vueTroll {
+	
+	Troll troll;
+	
+	public vueTroll(Troll t) {
+		this.troll = t;
+	}
+	
+	public void afficherInfos() {
+		
+	}
+	
+	public void afficherInventaire() {
+		
+	}
+	
+	public void afficherEquipement() {
+		
+	}
+
+}
